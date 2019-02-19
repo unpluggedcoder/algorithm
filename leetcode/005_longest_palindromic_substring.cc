a2fb95979f687ff3dd9d2de7e2d452ff1f753b47
unpluggedcoder <unpluggedcoder@outlook.com>
2019-02-19 13:04:26 +0800

    Finish 005_longest_palindromic_substring

--- leetcode/005_longest_palindromic_substring.cc
+++ leetcode/005_longest_palindromic_substring.cc
@@ -9,33 +9,41 @@ class Solution {
     string longestPalindrome(string s) {
         string es = "$#";
         for (auto it = s.begin(); it != s.end(); it++) {
-            es += *it + "#";
+            es += *it;
+            es += "#";
         }
-        int *RL = new int[s.size()];
+        int *RL = new int[es.length()];
         int maxRight = 0;
-        int maxLen = 0;
         int pos = 0;
-        size_t len = es.size();
+        int maxLen = 0;
+        int maxPos = 0;
+        size_t len = es.length();
         for (int i = 0; i < len; i++) {
             if (i >= maxRight)
                 RL[i] = 1;
             else
                 RL[i] = min(RL[2 * pos - i], maxRight - i);
             while (i - RL[i] >= 0 && i + RL[i] < len &&
-                   s[i - RL[i]] == s[i + RL[i]])
+                   es[i - RL[i]] == es[i + RL[i]])
                 RL[i]++;
             if ((RL[i] + i - 1) > maxRight) {
                 maxRight = RL[i] + i - 1;
                 pos = i;
             }
-            maxLen = max(maxLen, RL[i]);
+            if (RL[i] > maxLen) {
+                maxLen = RL[i];
+                maxPos = i;
+            }
         }
         delete[] RL;
-        return maxLen - 1;
+        maxPos = (maxPos / 2) - 1;
+        return s.substr(maxPos - (maxLen / 2) + 1, maxLen - 1);
     }
 };
 
 int main(int argc, char const *argv[]) {
-    /* code */
+    string s = "bbababc";
+    string pal = Solution().longestPalindrome(s);
+    cout << s << " longest palindrome length: " << pal << endl;
     return 0;
 }
